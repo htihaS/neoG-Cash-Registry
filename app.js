3216ea8681c3d0c29d99ff6550c1b04597af54c4
= <=>
2022-08-22 21:13:43 +0530

    added bonus and styling

--- app.js
+++ app.js
@@ -1,10 +1,15 @@
 const billAmount = document.querySelector(".bill-amount");
+const nextbutton = document.querySelector(".next")
 const cashReceived = document.querySelector(".cash-received");
 const checkButton = document.querySelector(".submit");
 const message = document.querySelector(".message");
+const nextbuttondiv = document.querySelector(".after-next-button");
 const availableNotes = [2000,500,100,20,10,5,1];
 const noOfNotes = document.querySelectorAll(".no-of-notes");
 
+nextbutton.addEventListener("click",()=>{
+    nextbuttondiv.style.display = "block";
+})
 
 checkButton.addEventListener("click",()=>{
     if(billAmount.value>0){
